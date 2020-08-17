da62d88d6b929a55bf60710d078051ce42d948ad
lockette <alperenerdogan0505@gmail.com>
2020-08-17 10:41:50 +0300

    auth db setup, request validation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,36 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
-
+import mongoose from 'mongoose';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handlers';
+import { NotFoundError } from './errors/not-found-error';
 const app = express();
 app.use(json());
-
-app.listen(3000, () => {
-  console.log('listening on 3000');
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
+app.use(errorHandler);
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('listening on 3000!');
+  });
+};
+start();
